b252fa82cebb730a3aed52a1eb6068e4a00b6b8c
Andy Lin <hcl.tku@gmail.com>
2014-02-24 21:33:19 +0800

    BMI隨堂作業

--- 助教課/BMI.cpp
+++ 助教課/BMI.cpp
@@ -4,36 +4,35 @@
 
 using namespace std;
 int main(){
-
 int w;
 
 double h,BMI,man,woman;
 
-cout<<"輸入身高體重知BMI想體重的程式 \n";
+	cout<<"輸入身高體重知BMI想體重的程式 \n";
 
-cout<<"身高(cm)為:";
+	cout<<"身高(cm)為:";
 
-cin>>h;
+	cin>>h;
 
-cout<<"體重(kg)為:";
+	cout<<"體重(kg)為:";
+	
+	cin>>w;
 
-cin>>w;
+	BMI=w/pow(h/100,2);
 
-BMI=w/pow(h/100,2);
+	man=(h-80)*0.7;
 
-man=(h-80)*0.7;
+	woman=(h-70)*0.6;
 
-woman=(h-70)*0.6;
+	cout<<"BMI值為:"<<BMI<<"\n";
 
-cout<<"BMI值為:"<<BMI<<"\n";
+		if(BMI<20) cout<<"太瘦了!!\n";
 
-if(BMI<20) cout<<"太瘦了!!\n";
+	if(BMI>=20 && BMI<=25) cout<<"正常\n";
 
-if(BMI>=20 && BMI<=25) cout<<"正常\n";
+	if(BMI>25 && BMI<=28) cout<<"過重\n";
 
-if(BMI>25 && BMI<=28) cout<<"過重\n";
-
-if(BMI>28) cout<<"肥胖\n";
+	if(BMI>28) cout<<"肥胖\n";
 
 return 0;
 
