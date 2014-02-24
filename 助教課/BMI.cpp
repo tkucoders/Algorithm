70f1bff2035052426790b46ec666afe401abde81
Andy Lin <hcl.tku@gmail.com>
2014-02-24 21:34:09 +0800

    BMI隨堂作業

--- 助教課/BMI.cpp
+++ 助教課/BMI.cpp
@@ -28,11 +28,11 @@ double h,BMI,man,woman;
 
 		if(BMI<20) cout<<"太瘦了!!\n";
 
-	if(BMI>=20 && BMI<=25) cout<<"正常\n";
+		if(BMI>=20 && BMI<=25) cout<<"正常\n";
 
-	if(BMI>25 && BMI<=28) cout<<"過重\n";
+		if(BMI>25 && BMI<=28) cout<<"過重\n";
 
-	if(BMI>28) cout<<"肥胖\n";
+		if(BMI>28) cout<<"肥胖\n";
 
 return 0;
 
